ef7bd4ee4f5f4311faf633a5f2cf395a5ef9ec8d
Amit Bhalla <amit9815@gmail.com>
2021-10-22 23:02:03 +0530

    Expense Item contd.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,17 +2,13 @@ import React from 'react';
 
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 20);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toUTCString()}</div>
+      <div>{props.date.toUTCString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
